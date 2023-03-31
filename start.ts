8b6878fa43ca5680f2782d8921b137fad8260869
pung <fp94214@gmail.com>
2023-03-31 11:51:43 +0800

    ～

--- start.ts
+++ start.ts
@@ -1,19 +1,18 @@
-
 import { Processor } from '@ugdu/processor'
 import { serve } from '@ugdu/packer'
 
 import Vue from '@vitejs/plugin-vue'
 
-
-new Processor().task(serve).hook('get-config', () => {
-  return {
-    apps: [
-      { name: '@enocloud/container', packages: (lps) => lps.map((lp) => lp.name) }
-    ],
-    extensions: ['vue', 'ts'],
-    meta: 'local',
-    vite: {
-      plugins: [Vue()]
-    }
-  }  as any
-}).run()
+new Processor()
+  .task(serve)
+  .hook('get-config', () => {
+    return {
+      apps: [{ name: '@pnpm8/container', packages: (lps) => lps.map((lp) => lp.name) }],
+      extensions: ['vue', 'ts'],
+      meta: 'local',
+      vite: {
+        plugins: [Vue()],
+      },
+    } as any
+  })
+  .run()
